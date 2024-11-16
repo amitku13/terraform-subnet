1a41d0d7607f655f7f1c33cd85cb46747e3ba951
amitku13 <amitkumarbhontele@gmail.com>
2024-11-17 02:37:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 module "vpc" {
-  source      = "git::https://github.com/amitku13/vpc.git?ref=main"
-  cidr_block  = "10.0.0.0/16"     # Correct variable name
-  enable_dns  = true              # Correct variable name (if applicable)
+  source = "git::https://github.com/amitku13/vpc.git?ref=main"
+
+  # Replace these with the correct names expected by the module
+  cidr_block          = "10.0.0.0/16"   # Example: if the module expects 'cidr_block'
+  enable_dns_support  = true            # Example: if the module uses 'enable_dns_support'
 }
